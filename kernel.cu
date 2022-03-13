5f19d201fc0a0c20b8a663e483ac6aebada31a89
Daniel Wong <danx255@gmail.com>
2022-03-13 14:46:56 -0700

    Initial commit

---
+++ kernel.cu
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+__global__ void histo_kernel(unsigned int* input, unsigned int* bins, unsigned int num_elements, unsigned int num_bins)
+{
+	
+    /*************************************************************************/
+    // INSERT KERNEL CODE HERE
+	
+	
+	  /*************************************************************************/
+}
+
+void histogram(unsigned int* input, unsigned int* bins, unsigned int num_elements, unsigned int num_bins) {
+
+	  /*************************************************************************/
+    //INSERT CODE HERE
+
+
+	  /*************************************************************************/
+
+}
+
+
